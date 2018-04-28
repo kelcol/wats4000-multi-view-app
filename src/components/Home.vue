4088b0ace77d4ecad4b50f925cdb80bcba5bf0c3
Kelly Colht <colhk@seattleu.edu>
2018-04-28 13:21:46 -0700

    Changed up success messaging

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,13 +10,39 @@
     <div v-show="showForm" class="form-container container card bg-info text-white mb-3">
       <h1 class="card-header">Join the Web Developers Club!</h1>
       <p>Sign up to access our special, secret page. Just create an account and answer a brief survey.</p>
+
+
+    <div class="alert alert-dismissible alert-danger" v-show="showError">
+      <button type="button" class="close" data-dismiss="alert">&times;</button>
+      <strong>Dag! You broke it!</strong>
+      <a href="#" class="alert-link">Change a few things up</a> and try submitting again.
+    </div>      
+
+
+      <div class="alert alert-dismissible alert-success" v-show="showSuccess">
+  
+  
+  <button type="button" class="close" data-dismiss="alert">&times;</button>
+
+
       
-      <p id="error" v-show="showError" class="">Dag! You broke it!</p>
+      <p><strong>Thank you for signing up!</strong> Please take our new member survey.<br><br><router-link to="Survey" class="btn btn-warning btn-md text-white" type="button">Click here</router-link></p>
+
+</div>
+
+      <div v-show="showModal" class="container card bg-info text-white mb-3">
+      <div class="card-header"><h3>Thank you for signing up!</h3></div>
+      <div class="card-body">
+      <p class="card-text">Please take our new member survey.<br><br><router-link to="Survey" class="btn btn-warning btn-md text-white" type="button">Click here</router-link></p>
+      </div>
+    </div>
+
+
 
       <form v-on:submit.prevent="validateForm">
 
         <p>
-          <label for="username">Usernames
+          <label for="username">Username
             <input class="form-control mr-sm-2" type="text" id="username"v-model="username">
           </label>
         </p>
@@ -38,14 +64,6 @@
         <p><input class="btn btn-primary" type="submit" value="Submit"></p>
       </form>
     </div>
-    
-    <!--TODO: Make this a modal -->
-    <div v-show="showModal" class="container card bg-info text-white mb-3">
-      <div class="card-header"><h3>Thank you for signing up!</h3></div>
-      <div class="card-body">
-      <p class="card-text">Please take our new member survey.<br><br><router-link to="Survey" class="btn btn-warning btn-md text-white" type="button">Click here</router-link></p>
-      </div>
-    </div>
 
   </div>
 </template>
@@ -61,7 +79,7 @@ export default {
       passwordVerify: '',
       showForm: true,
       showError: false,
-      showModal: false
+      showSuccess: false
     }
   },
   methods: {
@@ -69,18 +87,16 @@ export default {
       if ((this.username !== '') && 
       (this.email !== '') &&
       (this.password === this.passwordVerify)) {
-        console.log("Form is valid");
-        // TODO: Assign bootstrap success class to div instead
-        this.showForm = false;
-        this.showModal = true;
+        console.log("Form is valid");        
+        this.showSuccess = true;
+        this.showError = false;
       } else {
         this.showForm = true;
-        // TODO: Assign bootstrap error class to div instead
         this.showError = true;
+        this.showSuccess = false;
         console.log("Form is not valid");
       }
     },
-
   }
 }
 </script>
